cc1dc59b18987cea2417f3eb4d7d8a1e3c11de08
Mohammad Moheb <mmoheb13@gmail.com>
2016-07-20 13:27:15 +0200

    Update that I have got from Maged today Via Facebook Chat

---
+++ platforms/browser/www/js/hide.js
@@ -0,0 +1,9 @@
+function hideDiv(divid){
+			document.getElementById(divid).className = document.getElementById(divid).className + " hide";
+			document.getElementById("arrow"+divid).innerHTML="<i class=\"fa fa-arrow-down fa-2x pull-right\" aria-hidden=\"true\" onclick=\"showDiv('"+divid+"')\"></i>";
+}
+
+function showDiv(divid){
+			document.getElementById(divid).className =  "";
+			document.getElementById("arrow"+divid).innerHTML="<i class=\"fa fa-arrow-up fa-2x pull-right\" aria-hidden=\"true\" onclick=\"hideDiv('"+divid+"')\"></i>";
+		}
